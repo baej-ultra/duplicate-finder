6046a6ae19dc7ac836dc045e762e7b902a4dbf4a
baej-ultra <pigmejov@gmail.com>
2025-02-28 15:19:04 +0100

    I think it's working

---
+++ src/main/java/org/baej/DuplicateFinder.java
@@ -0,0 +1,103 @@
+package org.baej;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class DuplicateFinder extends SimpleFileVisitor<Path> {
+
+    Set<Path> paths = new HashSet<>();
+
+    public DuplicateFinder(Path startDir) {
+        try {
+            Files.walkFileTree(startDir, this);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public Map<String, List<Path>> getDuplicates() {
+        // Get candidates based on file size
+        List<List<Path>> candidates = paths.stream()
+                .collect(Collectors.groupingBy(path -> {
+                    try {
+                        return Files.size(path);
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                }))
+                .values()
+                .stream()
+                .filter(entry -> entry.size() >= 2)
+                .toList();
+
+        // Get checksums of all candidates and filter to find duplicates
+        Map<String, List<Path>> duplicates = candidates.stream()
+                .flatMap(List::stream)
+                .collect(Collectors.groupingBy(path -> {
+                    try {
+                        return getChecksum(path);
+                    } catch (NoSuchAlgorithmException | IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                }))
+                .entrySet().stream()
+                .filter(entry -> entry.getValue().size() >= 2)
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+
+        return duplicates;
+    }
+
+    private String getChecksum(Path path) throws NoSuchAlgorithmException, IOException {
+        MessageDigest md = MessageDigest.getInstance("MD5");
+        ByteBuffer buffer = ByteBuffer.allocate(4096);
+
+        // Read file and update the message digest
+        try (FileChannel fc = FileChannel.open(path)) {
+            while (fc.read(buffer) != -1) {
+                buffer.flip();
+                md.update(buffer);
+                buffer.clear();
+            }
+        }
+
+        // Convert byte array to hex string
+        var digest = md.digest();
+        StringBuilder sb = new StringBuilder();
+        for (byte b : digest) {
+            sb.append(String.format("%02x", b));
+        }
+
+        return sb.toString();
+    }
+
+    @Override
+    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+        paths.add(file);
+
+        return FileVisitResult.CONTINUE;
+    }
+
+    @Override
+    public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
+        System.out.println("Could not access " + file);
+
+        return FileVisitResult.CONTINUE;
+    }
+
+    @Override
+    public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+        System.out.println("Checking directory... " + dir.toAbsolutePath());
+
+        return FileVisitResult.CONTINUE;
+    }
+}
---
+++ src/main/java/org/baej/Main.java
@@ -0,0 +1,50 @@
+package org.baej;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.security.NoSuchAlgorithmException;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Map;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException, NoSuchAlgorithmException {
+
+        Path rootPath = Path.of(args[0]);
+
+        if (!Files.isDirectory(rootPath)) {
+            System.err.println("Not a directory.");
+        }
+
+        DuplicateFinder finder = new DuplicateFinder(rootPath);
+
+        var duplicates = finder.getDuplicates();
+
+        if (!duplicates.isEmpty()) {
+            System.out.print("\nDuplicates found!\n");
+            writeToFile(Path.of("./duplicates_%s.txt".formatted(
+                    LocalDateTime.now().toString().replaceAll("[.:-]","_"))), duplicates);
+        } else {
+            System.out.println("\nDuplicates not found.");
+        }
+    }
+
+    private static void writeToFile(Path location, Map<String, List<Path>> duplicates) throws IOException {
+        try (BufferedWriter bw =
+                     Files.newBufferedWriter(location, StandardOpenOption.TRUNCATE_EXISTING, StandardOpenOption.CREATE)) {
+            for (Map.Entry<String, List<Path>> entry : duplicates.entrySet()) {
+                bw.write(entry.getKey());
+                bw.newLine();
+                for (Path path : entry.getValue()) {
+                    bw.write(path.toAbsolutePath().toString());
+                    bw.newLine();
+                }
+            }
+        }
+    }
+
+}
